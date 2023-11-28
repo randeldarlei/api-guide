a659cb3a3d5d44894097f2219e8be6b4891975c6
Darlei Randel Oliveira Menezes <darley_rom@hotmail.com>
2023-11-28 18:49:45 -0300

    Add second method

--- python-api.py
+++ python-api.py
@@ -11,9 +11,16 @@ def homepage():
 # API functons
 @app.route('/get_sales')
 def get_sales():
-    table = pd.read_csv('advertising.csv')
+    table = pd.read_csv('inventario.csv')
     sales = table['Vendas'].sum()
     response = {'sales': sales}
+    return jsonify(response)
+
+@app.route('/get_total_products')
+def get_total_products():
+    table = pd.read_csv('inventario.csv')
+    total_products = table['Produtos'].sum()
+    response = {'total_products': total_products}
     return jsonify(response)
 
 # Execute Flask app
