f27ce33504c83d0e0849f0c95568f5db4bc49a43
Darlei Randel Oliveira Menezes <darley_rom@hotmail.com>
2024-03-15 15:52:09 -0300

    Fix app.route

--- src/python-api.py
+++ src/python-api.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'Hello World!!!'
+    return 'Homepage!!!'
 
 # API functons
-@app.route('/get_sales')
+@app.route('/sales')
 def get_sales():
     table = pd.read_csv('inventario.csv')
     sales = table['Vendas'].sum()
-    response = {'sales': sales}
+    response = {'sales' : int(sales)}
     return jsonify(response)
 
-@app.route('/get_total_products')
+@app.route('/total_products')
 def get_total_products():
     table = pd.read_csv('inventario.csv')
     total_products = table['Produtos'].sum()
-    response = {'total_products': total_products}
+    response = {'total_products' : int(total_products)}
     return jsonify(response)
 
-@app.route('/get_total_stock')
+@app.route('/total_stock')
 def get_total_stock():
     table = pd.read_csv('inventario.csv')
     total_stock = table['Estoque'].sum()
-    response = {'total_stock': total_stock}
+    response = {'total_stock' : int(total_stock)}
     return jsonify(response)
 
 # Execute Flask app
